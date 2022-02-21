b4a2a5024569390db37515c7b3a4de70596daac9
Matthew Nelson <developer@matthewnelson.io>
2022-02-20 20:30:45 -0500

    Prepare for next development iteration

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,11 +62,11 @@ plugins {
 
 kmpPublish {
     setupRootProject(
-        versionName = "0.4.6.10",
+        versionName = "0.4.6.11-SNAPSHOT",
         //    0.4.6.9  == 00_04_06_09_00
         //    0.4.6.9a == 00_04_06_09_01
         //    0.4.6.9b == 00_04_06_09_02
-        versionCode = /*00_0*/4_06_10_00,
+        versionCode = /*00_0*/4_06_11_00,
         pomInceptionYear = 2021,
     )
 }
