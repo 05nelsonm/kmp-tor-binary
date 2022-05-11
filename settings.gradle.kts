62ead02f9424a334611c6f54aa94b2555f47fbcd
Matthew Nelson <developer@matthewnelson.io>
2022-05-10 20:21:02 -0400

    Fix check publication functionality

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,17 +3,17 @@ rootProject.name = "kmp-tor-binary"
 includeBuild("kotlin-components/includeBuild/dependencies")
 includeBuild("kotlin-components/includeBuild/kmp")
 
-include(":library:kmp-tor-binary-android")
-include(":library:kmp-tor-binary-extract")
-include(":library:kmp-tor-binary-geoip")
-include(":library:kmp-tor-binary-linuxx64")
-include(":library:kmp-tor-binary-linuxx86")
-include(":library:kmp-tor-binary-macosx64")
-include(":library:kmp-tor-binary-mingwx64")
-include(":library:kmp-tor-binary-mingwx86")
-
 @Suppress("PrivatePropertyName")
 private val CHECK_PUBLICATION: String? by settings
 if (CHECK_PUBLICATION != null) {
     include(":tools:check-publication")
+} else {
+    include(":library:kmp-tor-binary-android")
+    include(":library:kmp-tor-binary-extract")
+    include(":library:kmp-tor-binary-geoip")
+    include(":library:kmp-tor-binary-linuxx64")
+    include(":library:kmp-tor-binary-linuxx86")
+    include(":library:kmp-tor-binary-macosx64")
+    include(":library:kmp-tor-binary-mingwx64")
+    include(":library:kmp-tor-binary-mingwx86")
 }
