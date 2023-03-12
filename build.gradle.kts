9411715e37374c8efae8b5da9aeb35919750f0f0
Matthew Nelson <developer@matthewnelson.io>
2023-03-12 12:09:19 -0400

    Add binary validation and dependency update plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
@@ -20,6 +21,8 @@ import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 plugins {
     alias(libs.plugins.multiplatform) apply(false)
     alias(libs.plugins.android.library) apply(false)
+    alias(libs.plugins.binaryCompat)
+    alias(libs.plugins.gradleVersions)
 }
 
 allprojects {
@@ -38,4 +41,56 @@ allprojects {
 
 plugins.withType<YarnPlugin> {
     the<YarnRootExtension>().lockFileDirectory = rootDir.resolve(".kotlin-js-store")
+}
+
+@Suppress("LocalVariableName")
+apiValidation {
+    val KMP_TARGETS = findProperty("KMP_TARGETS") as? String
+    val CHECK_PUBLICATION = findProperty("CHECK_PUBLICATION") as? String
+    val KMP_TARGETS_ALL = System.getProperty("KMP_TARGETS_ALL") != null
+    val TARGETS = KMP_TARGETS?.split(',')
+
+    if (CHECK_PUBLICATION != null) {
+        ignoredProjects.add("check-publication")
+    } else {
+        val JVM = TARGETS?.contains("JVM") != false
+        val ANDROID = TARGETS?.contains("ANDROID") != false
+
+        // Don't check these projects when building JVM only or Android only
+        if (!KMP_TARGETS_ALL && ((!ANDROID && JVM) || (ANDROID && !JVM))) {
+            ignoredProjects.add("kmp-tor-binary-android")
+            ignoredProjects.add("kmp-tor-binary-extract")
+            ignoredProjects.add("kmp-tor-binary-geoip")
+        }
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    // Example 1: reject all non stable versions
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+
+    // Example 2: disallow release candidates as upgradable versions from stable versions
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+
+    // Example 3: using the full syntax
+    resolutionStrategy {
+        componentSelection {
+            all {
+                if (isNonStable(candidate.version) && !isNonStable(currentVersion)) {
+                    reject("Release candidate")
+                }
+            }
+        }
+    }
 }
