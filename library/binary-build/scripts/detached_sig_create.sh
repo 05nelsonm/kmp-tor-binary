b6918a58439b33cbe2a07829c9eafb17b8fff1b3
Matthew Nelson <developer@matthewnelson.io>
2023-03-26 12:42:35 -0400

    Add detached signature generation for windows binaries (#80)

--- library/binary-build/scripts/detached_sig_create.sh
+++ library/binary-build/scripts/detached_sig_create.sh
@@ -28,6 +28,10 @@ readonly TOOLING="$DIR_PROJECT/tooling"
 readonly CMD_MACOS="macos"
 readonly CMD_MINGW="mingw"
 
+# Programs
+readonly RCODESIGN=$(which rcodesign)
+readonly OSSLSIGNCODE=$(which osslsigncode)
+
 help() {
   echo "
     $0
@@ -62,30 +66,40 @@ initialize() {
 
 macos() {
   initialize
-  check_rcodesign
 
+  if [ "$RCODESIGN" = "" ]; then
+    echo "
+    ERROR: Apple Codesign is required to be installed to run this script
+           See https://gregoryszorc.com/docs/apple-codesign/main/apple_codesign_getting_started.html#installing
+    "
+    exit 1
+  fi
+
+  # Read in .p12 key file path
   printf "Path to .p12 key file (e.g. /home/user/dir/key.p12): "
   read -r PATH_P12_KEY
 
   if [ ! -f "$PATH_P12_KEY" ]; then
     echo "
-    File does not exist [$PATH_P12_KEY]
+    ERROR: File does not exist [$PATH_P12_KEY]
     "
     exit 1
   fi
 
-  printf "Path to App Store Connect api-key json file (e.g. /home/user/dir/api-key.json): "
+  # Read in App Store Connect apikey.json file path
+  printf "Path to App Store Connect api-key json file (e.g. /home/user/dir/api_key.json): "
   read -r PATH_API_KEY
 
   if [ ! -f "$PATH_API_KEY" ]; then
     echo "
-    File does not exist [$PATH_API_KEY]
+    ERROR: File does not exist [$PATH_API_KEY]
     "
     exit 1
   fi
 
   change_dir_or_exit "$DIR_MACOS"
 
+  # For all archives in built/macos ending in -unsigned.tar.gz
   for ARCHIVE in "$(pwd)"/*-unsigned.tar.gz; do
     DIR_SIGNATURES=$(echo "$ARCHIVE" | sed "s|-unsigned.tar.gz|-signatures|g")
     OUT="$DIR_SIGNATURES.tar.gz"
@@ -95,8 +109,11 @@ macos() {
     BUNDLE_UNSIGNED="$BUNDLE_MACOS/Unsigned"
 
     rm -rf "$BUNDLE"
+
+    # Create .app bundle dirs
     mkdir -p "$BUNDLE_TOR"
 
+    # Generate our Info.plist manifest
     touch "$BUNDLE/Contents/Info.plist"
     echo '<?xml version="1.0" encoding="UTF-8"?>
 <plist version="1.0">
@@ -110,6 +127,7 @@ macos() {
 </dict>
 </plist>' > "$BUNDLE/Contents/Info.plist"
 
+    # Extract unsigned archive contents to the Tor directory within the bundle
     ${TAR} -xz -C "$BUNDLE_TOR" -f "$ARCHIVE"
 
     # By copying and setting tor.program as the bundle executable, we ensure
@@ -122,14 +140,17 @@ macos() {
       exit 1
     fi
 
+    # Sign everything in the app bundle
     if ! ${RCODESIGN} sign --p12-file "$PATH_P12_KEY" --code-signature-flags runtime "$BUNDLE"; then
       exit 1
     fi
 
+    # Submit app bundle for notarization and stapling
     if ! ${RCODESIGN} notary-submit --api-key-path "$PATH_API_KEY" --staple "$BUNDLE"; then
       exit 1
     fi
 
+    # Extract unsigned archive contents again to diff agains
     mkdir -p "$BUNDLE_UNSIGNED"
     ${TAR} -xz -C "$BUNDLE_UNSIGNED" -f "$ARCHIVE"
     change_dir_or_exit "$BUNDLE_UNSIGNED"
@@ -138,6 +159,8 @@ macos() {
 
     change_dir_or_exit "$DIR_PROJECT"
 
+    # Create diffs between unsigned, and signed
+    # binaries (.signature files) to be applied later
     for FILE in $FILES; do
       ${TOOLING} diff-cli create --diff-ext-name ".signature" "$BUNDLE_UNSIGNED/$FILE" "$BUNDLE_TOR/$FILE" "$DIR_SIGNATURES"
     done
@@ -149,6 +172,7 @@ macos() {
     Created $OUT
     "
 
+    # Clean up
     rm -rf "$BUNDLE"
     rm -rf "$DIR_SIGNATURES"
 
@@ -159,10 +183,87 @@ macos() {
 
 mingw() {
   initialize
-  check_osslsigncode
+
+  if [ "$OSSLSIGNCODE" = "" ]; then
+    echo "
+    ERROR: osslsigncode is required to be installed to run this script
+           See https://github.com/mtrojnar/osslsigncode
+    "
+    exit 1
+  fi
+
+  # Read in .key file path
+  printf "Path to .key file (e.g. /home/user/dir/my_key.key): "
+  read -r PATH_KEY
+
+  if [ ! -f "$PATH_KEY" ]; then
+    echo "
+    ERROR: File does not exist [$PATH_KEY]
+    "
+    exit 1
+  fi
+
+  # Read in cert file path
+  printf "Path to cert file (e.g. /home/user/dir/cert.cer): "
+  read -r PATH_CERT
+
+  if [ ! -f "$PATH_CERT" ]; then
+    echo "
+    ERROR: File does not exist [$PATH_CERT]
+    "
+    exit 1
+  fi
 
   change_dir_or_exit "$DIR_MINGW"
-  # TODO
+
+  # For all archives in built/mingw ending in -unsigned.tar.gz
+  for ARCHIVE in "$(pwd)"/*-unsigned.tar.gz; do
+    DIR_SIGNATURES=$(echo "$ARCHIVE" | sed "s|-unsigned.tar.gz|-signatures|g")
+    OUT="$DIR_SIGNATURES.tar.gz"
+    DIR_SIGNED=$(echo "$ARCHIVE" | sed "s|-unsigned.tar.gz|-signed|g")
+    DIR_UNSIGNED=$(echo "$ARCHIVE" | sed "s|-unsigned.tar.gz|-unsigned|g" )
+
+    rm -rf "$DIR_SIGNED"
+    rm -rf "$DIR_UNSIGNED"
+    rm -rf "$DIR_SIGNATURES"
+    mkdir -p "$DIR_SIGNED"
+    mkdir -p "$DIR_UNSIGNED"
+
+    # Extract unsigned archive contents
+    ${TAR} -xz -C "$DIR_UNSIGNED" -f "$ARCHIVE"
+    change_dir_or_exit "$DIR_UNSIGNED"
+    # shellcheck disable=SC2035
+    FILES=$(${FIND} * -type f)
+
+    change_dir_or_exit "$DIR_PROJECT"
+
+    for FILE in $FILES; do
+      if ! ${OSSLSIGNCODE} sign -certs "$PATH_CERT" \
+           -key "$PATH_KEY" \
+           -t "http://timestamp.comodoca.com" \
+           -in "$DIR_UNSIGNED/$FILE" \
+           -out "$DIR_SIGNED/$FILE"; then
+        exit 1
+      fi
+
+      ${TOOLING} diff-cli create --diff-ext-name ".signature" "$DIR_UNSIGNED/$FILE" "$DIR_SIGNED/$FILE" "$DIR_SIGNATURES"
+    done
+
+    change_dir_or_exit "$DIR_SIGNATURES"
+    # shellcheck disable=SC2035
+    ${TAR} -cz * -f "$OUT"
+    echo "
+    Created $OUT
+    "
+
+    # Clean up
+    rm -rf "$DIR_SIGNATURES"
+    rm -rf "$DIR_SIGNED"
+    rm -rf "$DIR_UNSIGNED"
+
+    change_dir_or_exit "$DIR_MINGW"
+    sleep 1
+  done
 }
 
 case $1 in
