a5611045717a4050591dc1f3071fd0200de12a03
Matthew Nelson <developer@matthewnelson.io>
2023-03-27 11:51:39 -0400

    Implement linux/macos/mingw resource extraction

--- library/binary-build/scripts/resources_create.sh
+++ library/binary-build/scripts/resources_create.sh
@@ -137,15 +137,130 @@ geoips() {
   rm -rf "$DIR_BUILT/$CMD_GEOIPS"
 
   echo "
-    Geoip files have been copied to module kmp-tor-binary-geoip
+    Geoip files have been copied to
+    the kmp-tor-binary-geoip module
   "
 }
 
-binaries() {
-  # TODO
+check_architecture() {
+  case $1 in
+    "arm64"|"x64"|"x86")
+      return 0
+      ;;
+  esac
+
   echo "
-  $1
+    ERROR: Unknown architecture [$1]
   "
+  exit 1
+}
+
+binaries() {
+  change_dir_or_exit "$DIR_BUILT/$1"
+
+  for ARCHIVE in "$(pwd)"/*-unsigned.tar.gz; do
+    DIR_WORK=$(echo "$ARCHIVE" | sed "s|-unsigned.tar.gz||g")
+    ARCH=$(echo "$DIR_WORK" | rev | cut -d '/' -f 1 | rev | sed "s|tor-$1-||g")
+
+    check_architecture "$ARCH"
+
+    rm -rf "$DIR_WORK"
+    mkdir -p "$DIR_WORK"
+    ${TAR} -xz -C "$DIR_WORK" -f "$ARCHIVE"
+
+    change_dir_or_exit "$DIR_WORK"
+    # shellcheck disable=SC2035
+    FILES=$(${FIND} * -type f)
+
+    # Apply signatures for macos and windows binaries
+    if [ "$1" = "$CMD_MACOS" ] || [ "$1" = "$CMD_MINGW" ]; then
+      DIR_SIGNATURES="$DIR_WORK-signatures"
+
+      # Extract signatures
+      rm -rf "$DIR_SIGNATURES"
+      mkdir -p "$DIR_SIGNATURES"
+      ${TAR} -xz -C "$DIR_SIGNATURES" -f "$DIR_SIGNATURES.tar.gz"
+
+      change_dir_or_exit "$DIR_PROJECT"
+
+      # Apply signatures to each file
+      for FILE in $FILES; do
+        if ! ${TOOLING} diff-cli apply "$DIR_SIGNATURES/$FILE.signature" "$DIR_WORK/$FILE"; then
+          exit 1
+        fi
+
+        # Fix file permissions
+        chmod 700 "$DIR_WORK/$FILE"
+      done
+
+      # Clean up
+      change_dir_or_exit "$DIR_WORK"
+      rm -rf "$DIR_SIGNATURES"
+      sleep 1
+    fi
+
+    # Build sha256 values + file manifest list
+    FILES_MANIFEST=""
+    FILES_SHA256=""
+    for FILE in $FILES; do
+      H=$(SHA256 "$FILE")
+
+      if [ "$FILES_SHA256" = "" ]; then
+        FILES_MANIFEST="\"$FILE.gz\""
+        FILES_SHA256="$H"
+      else
+        FILES_MANIFEST="$FILES_MANIFEST, \"$FILE.gz\""
+        FILES_SHA256="$FILES_SHA256\n$H"
+      fi
+    done
+
+    # Take the sha256 hash of all the sha256 values combined
+    FILES_SHA256=$(echo "$FILES_SHA256" | ${OPENSSL} dgst -sha256 | rev | cut -d ' ' -f 1 | rev)
+
+    # Gzip directory contents
+    ${GZIP} -rn "$DIR_WORK"
+
+    SOURCE_SET="commonMain"
+
+    # macOS binaries go in jvmMain source set
+    if [ "$1" = "$CMD_MACOS" ]; then
+      SOURCE_SET="jvmMain"
+    fi
+
+    DIR_RESOURCES="$DIR/../../kmp-tor-binary-$1$ARCH/src/$SOURCE_SET/resources/kmptor/$1/$ARCH"
+
+    # Remove all old files and copy over new
+    rm -rf "$DIR_RESOURCES"
+    mkdir -p "$DIR_RESOURCES"
+    cp -ar . "$DIR_RESOURCES"
+
+    # Update TorResource.kt data for jvmJs
+    TOR_RESOURCE_KT_JVMJS="$DIR/../../kmp-tor-binary-extract/src/jvmJsMain/kotlin/io/matthewnelson/kmp/tor/binary/extract/TorResource.kt"
+    # shellcheck disable=SC2060
+    CONST_KT_TAG=$(echo "$1_$ARCH" | tr [a-z] [A-Z])
+
+    # Write sha256 & manifest values for jvmJs TorResource.kt
+    sed -i "s|/\* $CONST_KT_TAG \*/ override val resourceManifest: List<String> get() = .*|/\* $CONST_KT_TAG \*/ override val resourceManifest: List<String> get() = listOf($FILES_MANIFEST)|g" "$TOR_RESOURCE_KT_JVMJS"
+    sed -i "s|/\* $CONST_KT_TAG \*/ override val sha256sum: String get() = .*|/\* $CONST_KT_TAG \*/ override val sha256sum: String get() = \"$FILES_SHA256\"|g" "$TOR_RESOURCE_KT_JVMJS"
+
+    # Linux x64 & Mingw x64 binaries also have values that
+    # need to be updated for native TorResource.kt
+    if [ "$ARCH" = "x64" ]; then
+      if [ "$1" != "$CMD_MACOS" ]; then
+        TOR_RESOURCE_KT_NATIVE="$DIR/../../kmp-tor-binary-extract/src/$1X64Main/kotlin/io/matthewnelson/kmp/tor/binary/extract/TorResource.kt"
+        sed -i "s|/\* $CONST_KT_TAG \*/ override val resourceManifest: List<String> get() = .*|/\* $CONST_KT_TAG \*/ override val resourceManifest: List<String> get() = listOf($FILES_MANIFEST)|g" "$TOR_RESOURCE_KT_NATIVE"
+        sed -i "s|/\* $CONST_KT_TAG \*/ override val sha256sum: String get() = .*|/\* $CONST_KT_TAG \*/ override val sha256sum: String get() = \"$FILES_SHA256\"|g" "$TOR_RESOURCE_KT_NATIVE"
+      fi
+    fi
+
+    change_dir_or_exit "$DIR_BUILT/$1"
+    rm -rf "$DIR_WORK"
+
+    echo "
+    Tor binaries for $1[$ARCH] have been moved to
+    kmp-tor-binary-$1$ARCH module $SOURCE_SET/resources
+    "
+  done
 }
 
 case $1 in
