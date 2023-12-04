3b0b5c126e44a4a1628509b5352e5f3029618602
Matthew Nelson <developer@matthewnelson.io>
2023-12-04 10:54:28 -0500

    Add path_separator

--- library/binary-core/src/jsMain/kotlin/io/matthewnelson/kmp/tor/binary/core/internal/JsPlatform.kt
+++ library/binary-core/src/jsMain/kotlin/io/matthewnelson/kmp/tor/binary/core/internal/JsPlatform.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2022 Matthew Nelson
+ * Copyright (c) 2023 Matthew Nelson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         https://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,25 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-@file:JsModule("path")
-@file:JsNonModule
 @file:Suppress("FunctionName")
 
 package io.matthewnelson.kmp.tor.binary.core.internal
 
 import io.matthewnelson.kmp.tor.binary.core.InternalKmpTorBinaryApi
+import io.matthewnelson.kmp.tor.binary.core.OSHost
+import io.matthewnelson.kmp.tor.binary.core.OSInfo
 
-@JsName("sep")
-internal external val path_sep: String
-
-@JsName("dirname")
 @InternalKmpTorBinaryApi
-public external fun path_dirname(path: String): String
-
-@JsName("normalize")
-@InternalKmpTorBinaryApi
-public external fun path_normalize(path: String): String
-
-@JsName("resolve")
-@InternalKmpTorBinaryApi
-public external fun path_resolve(vararg path: String): String
+public val path_separator: Char by lazy {
+    try {
+        path_sep.first()
+    } catch (_: Throwable) {
+        if (OSInfo.INSTANCE.osHost is OSHost.Windows) {
+            '\\'
+        } else {
+            '/'
+        }
+    }
+}
