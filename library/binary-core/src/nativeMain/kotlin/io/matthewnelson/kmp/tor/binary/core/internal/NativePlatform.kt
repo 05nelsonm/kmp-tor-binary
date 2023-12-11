dd11acfd9718542bfea50fd2f925830003f413b3
Matthew Nelson <developer@matthewnelson.io>
2023-12-11 07:32:16 -0500

    WIP: extract to filesystem (no gunzip yet)

--- library/binary-core/src/nativeMain/kotlin/io/matthewnelson/kmp/tor/binary/core/internal/NativePlatform.kt
+++ library/binary-core/src/nativeMain/kotlin/io/matthewnelson/kmp/tor/binary/core/internal/NativePlatform.kt
@@ -15,44 +15,72 @@
  **/
 package io.matthewnelson.kmp.tor.binary.core.internal
 
-import io.matthewnelson.kmp.file.File
-import io.matthewnelson.kmp.file.IOException
-import io.matthewnelson.kmp.file.resolve
+import io.matthewnelson.kmp.file.*
 import io.matthewnelson.kmp.tor.binary.core.InternalKmpTorBinaryApi
 import io.matthewnelson.kmp.tor.binary.core.Resource
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.posix.errno
 
-@OptIn(InternalKmpTorBinaryApi::class)
+@OptIn(DelicateFileApi::class, ExperimentalForeignApi::class, InternalKmpTorBinaryApi::class)
 internal actual fun Resource.extractTo(destinationDir: File): File {
-    var fileName = platform.nativeResource.name
-    val isGzipped = if (fileName.endsWith(".gz")) {
-        fileName = fileName.substringBeforeLast(".gz")
-        true
+    val name = platform.nativeResource.name
+    val destFinal = if (name.endsWith(".gz")) {
+        destinationDir.resolve(name.substringBeforeLast(".gz"))
     } else {
-        false
+        null
     }
 
-    val destination = destinationDir.resolve(fileName)
+    // Could be gzipped, could not. Need to extract it to FS first
+    val dest = destinationDir.resolve(name)
 
-    if (destination.exists() && !destination.delete()) {
-        throw IOException("Failed to delete $destination")
+    if (dest.exists() && !dest.delete()) {
+        throw IOException("Failed to delete $dest")
     }
 
-    TODO("""
-        outputStream(destination).use { oStream ->
-          val out = if (isGzipped) {
-              gunzipStream { buf, len ->
-                  oStream.write(buf, 0, len)
-              }
-          } else {
-              oStream
-          }
-          //
-          nativeResource.read { buffer, len ->
-              out.write(buffer, 0, len)
-          }
+    if (destFinal != null && destFinal.exists() && !destFinal.delete()) {
+        throw IOException("Failed to delete $destFinal")
+    }
+
+    try {
+        dest.open(flags = "wb") { file ->
+            platform.nativeResource.read { buffer, len ->
+                val result = file.fWrite(buffer, 0, len)
+                if (result < 0) {
+                    throw errnoToIOException(errno)
+                }
+            }
         }
-    """)
+    } catch (t: Throwable) {
+        dest.delete()
+        throw t.wrapIOException()
+    }
 
+    if (destFinal == null) return dest
 
-    return destination
+    try {
+        // TODO: Implement gunzip instead of just copying
+        dest.open(flags = "rb") { file1 ->
+            destFinal.open(flags = "wb") { file2 ->
+                val buf = ByteArray(4096)
+
+                while (true) {
+                    val read = file1.fRead(buf)
+                    if (read < 0) throw errnoToIOException(errno)
+                    if (read == 0) break
+
+                    if (file2.fWrite(buf, len = read) < 0) {
+
+                        throw errnoToIOException(errno)
+                    }
+                }
+            }
+        }
+    } catch (t: Throwable) {
+        destFinal.delete()
+        throw t.wrapIOException()
+    } finally {
+        dest.delete()
+    }
+
+    return destFinal
 }
